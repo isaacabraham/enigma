559d2a4d711fb4d21ebf923cc5cfc00ba5008b7c
Isaac Abraham <isaac.abraham@gmail.com>
2025-03-02 20:59:48 +0000

    Reactive tests

--- src/Enigma/Tests.fs
+++ src/Enigma/Tests.fs
@@ -199,12 +199,12 @@ let allTests = testList "All Tests" [ unitTests; enigmaTests; propertyBasedTests
 
 [<EntryPoint>]
 let main args =
-    let solution =
-        Bombe.tryFindSolution
-            "EDPUD NRGYS ZRCXN UYTPO MRMBO FKTBZ REZKM LXLVE FGUEY SIOZV EQMIK UBPMM YLKLT TDEIS MDICA GYKUA CTCDO MOHWX MUUIA UBSTS LRNBZ SZWNR FXWFY SSXJZ VIJHI DISHP RKLKA YUPAD TXQSP INQMA TLPIF SVKDA SCTAC DPBOP VHJK"
-            "AUFKL"
-            (fun (m, progress, rate, elapsed, remaining) ->
-                printfn $"%.2f{progress}%% ({rate} sims/sec) {elapsed} elapsed, {remaining} remaining.")
+    // let solution =
+    //     Bombe.tryFindSolution
+    //         "EDPUD NRGYS ZRCXN UYTPO MRMBO FKTBZ REZKM LXLVE FGUEY SIOZV EQMIK UBPMM YLKLT TDEIS MDICA GYKUA CTCDO MOHWX MUUIA UBSTS LRNBZ SZWNR FXWFY SSXJZ VIJHI DISHP RKLKA YUPAD TXQSP INQMA TLPIF SVKDA SCTAC DPBOP VHJK"
+    //         "AUFKL"
+    //         (fun (m, progress, rate, elapsed, remaining) ->
+    //             printfn $"%.2f{progress}%% ({rate} sims/sec) {elapsed} elapsed, {remaining} remaining.")
 
-    0
-// runTestsWithCLIArgs [] args allTests
+    // 0
+    runTestsWithCLIArgs [] args allTests
